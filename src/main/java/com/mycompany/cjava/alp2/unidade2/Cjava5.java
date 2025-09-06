ae086d8cc16420f1fd1f59a80bf5b1f41aa21c7f
Rafael Zolet <zoletrafael101@gmail.com>
2025-09-06 00:07:19 -0300

    exercicios unidade 2

---
+++ src/main/java/com/mycompany/cjava/alp2/unidade2/Cjava5.java
@@ -0,0 +1,33 @@
+package com.mycompany.cjava.alp2.unidade2;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Cjava5 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Plano de Saúde");
+        System.out.println("Informe seu nome:");
+        String nome = entrada.next();
+        System.out.print("Informe sua Idade : \n -> ");
+        int idade = entrada.nextInt();
+
+        System.out.println(nome);
+        System.out.println(idade+"");
+        String msg = "O valor do plano para sua faixa etária de idade é R$ ";
+        if (idade > 0 && idade <= 18){
+            msg += 50;
+        } else if (idade >=19 && idade <= 29) {
+            msg += 70;
+        } else if (idade >= 30 && idade <= 45) {
+            msg += 90;
+        } else if (idade >= 46 && idade <= 65) {
+            msg += 130;
+        } else if (idade >= 65 && idade <= 120) {
+            msg += 170;
+        } else {
+            msg = "Idade informada é inválida, por favor verifique!";
+        }
+        System.out.println(msg);
+    }
+}
---
+++ src/main/java/com/mycompany/cjava/alp2/unidade2/Cjava6.java
@@ -0,0 +1,27 @@
+package com.mycompany.cjava.alp2.unidade2;
+
+import java.util.Scanner;
+
+public class Cjava6 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Eleições");
+        System.out.print("Digite seu nome :\n >");
+        String nome = entrada.next();
+        System.out.print("Digite sua idade : \n >");
+        int idade = entrada.nextInt();
+        String msg = "Prezado(a) " + nome + ", ";
+        if (idade < 15){
+            msg += "Você ainda não atingiu idade suficiente para poder se tornar um eleitor.";
+        } else if (idade >= 16 && idade < 18) {
+            msg += "Você já atingiu a idade para se tornar um eleitor, porém não é obrigatório \n " +
+                    "caso deseje, dirija-se até o cartório de sua cidade para emitir seu titulo.";
+        } else if (idade >= 18) {
+            msg += "Você já tem idade para se tornar um eleitor, de forma obrigatória perante a lei, \n" +
+                    "dirija-se até o cartório de sua cidade para emitir seu titulo.";
+        } else if (idade >= 65) {
+            msg = "Você não precisa mais se preocupar com obrigações eleitorais, aproveite e descanse.";
+        }
+        System.out.println(msg);
+    }
+}
